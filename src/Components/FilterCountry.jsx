16a90d9e84944c93c5f25ff740a7164d92310358
EasyCode <112218765+EasyCode01@users.noreply.github.com>
2023-08-08 12:59:30 +0100

    Update FilterCountry.jsx

--- src/Components/FilterCountry.jsx
+++ src/Components/FilterCountry.jsx
@@ -12,7 +12,7 @@ const FilterCountry = ({ handleFilterByChange }) => {
         <option value="America">America</option>
         <option value="Asia">Asia</option>
         <option value="Europe">Europe</option>
-        <option value="Oceania  ">Oceania</option>
+        <option value="Oceania">Oceania</option>
       </select>
     </div>
   );
